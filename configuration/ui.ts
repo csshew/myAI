d1a9844cb572a7fa3a7ac0b8bef04a4ac2449415
csshew <charles_shew@kenan-flagler.unc.edu>
2025-02-25 17:36:53 -0500

    changed all config ui.ts to my own

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Mr. Helpful`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI, Created by Charlie Shew`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CHAT_HEADER: string = `MBA835 Final Paper Assistant`;
+export const MESSAGE_PLACEHOLDER: string = `Hello! I'm an AI assistant created to help Charlie with writing his final paper for MBA835: Intro to Entreprenuership.`;
+export const FOOTER_MESSAGE: string = `Created by Charles Shew`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
+export const PAGE_TITLE: string = `AI Final Paper Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
