2551258e1d4a2d8148a71fac98e5aa3fe5bd9259
csshew <charles_shew@kenan-flagler.unc.edu>
2025-03-01 09:38:47 -0500

    Update ui.ts to RE

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `MBA835 Final Paper Assistant`;
-export const MESSAGE_PLACEHOLDER: string = `Hello! I'm an AI assistant created to help Charlie with writing his final paper for MBA835: Intro to Entreprenuership.`;
+export const CHAT_HEADER: string = `Milo AI`;
+export const MESSAGE_PLACEHOLDER: string = `Hello! I'm an AI assistant created to help you.`;
 export const FOOTER_MESSAGE: string = `Created by Charles Shew`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `AI Final Paper Assistant`;
+export const PAGE_TITLE: string = `Milo AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
